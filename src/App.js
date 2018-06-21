131781f79580846e83f54c192f1b0ce13151e806
Tony Carbetta <ruquss@gmail.com>
2018-06-21 14:19:39 -0700

    user can now successfully search for article and get it's link, however I'm working on returning the image

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ import axios from "axios";
 class App extends Component {
   state = {
     apikey: "8437b363971849dea95b19598d4c8fcd",
-    articles: []
+    articles: [],
+    userSearch: ''
+  }
+
+  userChange = (e) =>{
+    this.setState({userSearch: e.target.value})
+    // console.log(this.state.userSearch);
   }
 
   getArticles = () => {
-    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8437b363971849dea95b19598d4c8fcd&q=bacon").then(response => {
-      console.log(response.data.response.docs);
+    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=8437b363971849dea95b19598d4c8fcd&q=" + this.state.userSearch).then(response => {
+      // console.log(response.data.response.docs);
       this.setState({articles: response.data.response.docs})
     })
   }
 
   render() {
     return (<div className="App">
-      <h1>NY TIMES SEARCH</h1>
-      <button onClick={this.getArticles} className="btn btn-primary">Click for Articles</button>
+      <h1>NEW YORK TIMES <br /> ARTICLE SEARCH</h1>
+
+      <input type="text"
+            onChange={this.userChange}
+            value={this.state.userSearch}
+            placeholder="search for..."
+      />
+      <br />
+      <button onClick={this.getArticles} className="btn btn-primary m-3">Click for Articles</button>
       {
         this.state.articles.map((article, idx) => {
           return (<div key={idx}>
             <p>{article.snippet}</p>
             <a href={article.web_url} target="_blank">Link</a>
+            <img src={article.multimedia[idx].url} alt="userSearch" />
           </div>);
         })
       }
